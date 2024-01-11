b466a45f5a251a7bba84dbb6e245a4a7e7de074f
jenniferzouak <279472+jenniferzouak@users.noreply.github.com>
2024-01-11 14:57:54 -0700

    add image

--- concat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script removes the finalfile.txt if it exists, 
-# then iterates over Markdown files in the src directory, 
-# prepends a "#" and the filename (without extension) to each file's contents,
-# and appends them to finalfile.txt.
-
-rm -f README.md && for f in src/*.md; do (cat "${f}"; echo ;) >> README.md; done
